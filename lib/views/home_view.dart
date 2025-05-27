308bf18e09cf3b85c8b870c0e59693f4c7c3a396
Movermans1 <619401@olz.nl>
2025-05-27 11:22:11 +0200

    d

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import '../utils/xp_manager.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final XPManager xpManager = XPManager();
+
+  String get treeImage {
+    if (xpManager.xp < 50) {
+      return 'assets/images/tree_level1.png';
+    } else if (xpManager.xp < 100) {
+      return 'assets/images/tree_level2.png';
+    } else {
+      return 'assets/images/tree_level3.png';
+    }
+  }
+
+  void _completeTask() {
+    setState(() {
+      xpManager.addXP(20);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,23 +33,24 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('LifeXP'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welkom bij LifeXP!',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Naar takenlijst navigeren
-              },
-              child: const Text('Takenlijst'),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'XP: ${xpManager.xp}',
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          Image.asset(
+            treeImage,
+            height: 200,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _completeTask,
+            child: const Text('Taak voltooien (+20 XP)'),
+          ),
+        ],
       ),
     );
   }
